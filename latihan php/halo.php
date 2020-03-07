3aadfc02a6afbc46fae69c7a4f41483b5523bb5a
Farsy17 <aresfrsy@gmail.com>
2020-03-07 10:53:40 +0700

    baru

---
+++ latihan php/halo.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>PHP</title>
+</head>
+<body>
+		<P>
+			<?php 
+			echo "Hello velas !!";
+			echo " <br> ";
+			echo "Hari ini cerah";
+			echo "<br>";
+			echo "mari jalan-jalan";
+			?>
+		</P>
+		<p><?= "Apa kabar ?";?></p>
+</body>
+</html>
